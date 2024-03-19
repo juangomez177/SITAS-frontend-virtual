c5953bdfc80911b15ee35422f57212e4837aec62
Julian Rodriguez <julian@innovati.com.co>
2024-03-18 20:42:42 -0500

    Se crear fichero para validación de password

---
+++ utils/vliadate-password.ts
@@ -0,0 +1,6 @@
+
+const validatePassword = (password: string): boolean => {
+   console.log("Esto valida el password del usuario");
+   //TODO: Implementar la validación del password mediante expresiones regulares
+   return true;
+}
