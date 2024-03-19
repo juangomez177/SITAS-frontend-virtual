70fce913f321e021d29ef23600f8620803c7ba80
ElKev117 <kevin.ruda@udea.edu.co>
2024-03-18 22:36:18 -0500

    Se creo expresion regular para validar password

--- utils/vliadate-password.ts
+++ utils/vliadate-password.ts
@@ -1,6 +1,10 @@
 
-const validatePassword = (password: string): boolean => {
-   console.log("Esto valida el password del usuario");
-   //TODO: Implementar la validación del password mediante expresiones regulares
-   return true;
+export const validatePassword = (password: string): boolean => {
+  console.log("Esto valida el password del usuario");
+  let valid = false;
+  var exp = /^(?!\s)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%*?&])([A-Za-z\d#$@!%*?&]|[^ ]){8,25}$/;
+  if (exp.test(password)) {
+    valid = true;
+  }
+  return valid;
 }
