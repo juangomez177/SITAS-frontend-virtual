64a82a3bfca19802c79ca8e44ad596adf3367da2
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2024-04-16 23:30:54 -0500

    Se termina sección de filters

--- app/gestion/lista/page.tsx
+++ app/gestion/lista/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 
+import { Fragment } from "react"
+
 import Filters from "components/molecules/filters/Filters"
 import Table from "components/molecules/table/Table"
-
-import { Fragment } from "react"
 
 export default function ListadoGestion() {
   return (
