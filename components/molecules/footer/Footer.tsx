a170f4777ccf63ab382daaac47330e1bc3f1ece5
Jose Bernal <xjosex142@gmail.com>
2024-04-16 23:32:22 -0500

    Se crea el Navbar y el Footer

--- components/molecules/footer/Footer.tsx
+++ components/molecules/footer/Footer.tsx
@@ -1,7 +1,21 @@
+import PlaneIcon from "app/static/icons/Plane"
 import React from "react"
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="fixed bottom-0 w-full">
+      <div className=" bg-white">
+        <div className="container mx-auto flex flex-col items-center justify-between space-y-4 p-6 sm:flex-row sm:space-y-0">
+          <a href="#">
+            <h1 className="italic">Singapur Airlines</h1>
+          </a>
+          <p>© Copyright 2021. All Rights Reserved.</p>
+
+          <PlaneIcon />
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
--- components/molecules/navbar/Navbar.tsx
+++ components/molecules/navbar/Navbar.tsx
@@ -1,7 +1,20 @@
+import UserIcon from "app/static/icons/User"
 import React from "react"
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <nav className="relative bg-white shadow dark:bg-gray-800">
+      <div className="container px-6 py-4 mx-auto">
+        <div className="lg:flex lg:items-center lg:justify-between">
+          <a href="#">
+            <h1 className="italic">Singapur Airlines</h1>
+          </a>
+
+          <UserIcon />
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
