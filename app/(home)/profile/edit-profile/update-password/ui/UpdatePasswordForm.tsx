2712810ae542f0b77aa6a7b23441a654b9aa3011
ElKev117 <kevin.ruda@udea.edu.co>
2024-04-20 23:12:36 -0500

    Implementando el componente PasswordField en form de cambio  contraseña

--- app/(home)/profile/edit-profile/update-password/ui/UpdatePasswordForm.tsx
+++ app/(home)/profile/edit-profile/update-password/ui/UpdatePasswordForm.tsx
@@ -1,12 +1,10 @@
 "use client"
-import Visibility from "@mui/icons-material/Visibility"
-import VisibilityOff from "@mui/icons-material/VisibilityOff"
-import { Grid, TextField } from "@mui/material"
-import IconButton from "@mui/material/IconButton"
-import InputAdornment from "@mui/material/InputAdornment"
+
+import { Grid } from "@mui/material"
 import React, { useState } from "react"
 import { SubmitHandler, useForm } from "react-hook-form"
 import { passwordValidations } from "utils"
+import PasswordField from "../../../../../../components/ui/PasswordField"
 
 type FormInputs = {
   currentPassword: string
@@ -24,65 +22,40 @@ export default function UpdatePasswordForm() {
   const newPassword = watch("newPassword")
   const currentPassword = watch("currentPassword")
 
-  const [showPassword, setShowPassword] = useState(false)
-  const [showPassword2, setShowPassword2] = useState(false)
-  const [showPassword3, setShowPassword3] = useState(false)
+  const [showPassword, setShowPassword] = useState({ current: false, new: false, confirm: false })
 
-  const handleClickShowPassword = () => setShowPassword((show) => !show)
-  const handleClickShowPassword2 = () => setShowPassword2((show) => !show)
-  const handleClickShowPassword3 = () => setShowPassword3((show) => !show)
+  const handleClickShowPassword = (field: "current" | "new" | "confirm") => {
+    setShowPassword({ ...showPassword, [field]: !showPassword[field] })
+  }
 
   const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
   }
-  const handleMouseDownPassword2 = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault()
-  }
-  const handleMouseDownPassword3 = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault()
-  }
+
   const onSubmit: SubmitHandler<FormInputs> = async (data) => {
     console.log(data)
   }
+
   return (
     <form action="#">
       <Grid container={true} spacing={6}>
-        <Grid item xs={12} md={6} lg={12}>
-          <TextField
-            style={{ marginTop: "5%", marginBottom: "0" }}
+        <Grid item xs={12} md={6} lg={12} style={{ marginTop: "5%" }}>
+          <PasswordField
             label="Contraseña actual"
-            variant="outlined"
-            fullWidth
-            type={showPassword ? "text" : "password"}
-            {...register("currentPassword", {
+            register={register("currentPassword", {
               required: "Este campo es requerido",
               validate: passwordValidations.isPassword,
             })}
-            error={!!errors.currentPassword}
-            helperText={errors.currentPassword?.message}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
+            errors={errors.currentPassword}
+            showPassword={showPassword.current}
+            handleClickShowPassword={() => handleClickShowPassword("current")}
+            handleMouseDownPassword={handleMouseDownPassword}
           />
         </Grid>
         <Grid item xs={12} md={6} lg={12}>
-          <TextField
-            label="Nueva contraseña "
-            variant="outlined"
-            fullWidth
-            type={showPassword2 ? "text" : "password"}
-            {...register("newPassword", {
+          <PasswordField
+            label="Nueva contraseña"
+            register={register("newPassword", {
               required: "Este campo es requerido",
               validate: (value) => {
                 if (passwordValidations.isPassword(value)) {
@@ -94,31 +67,16 @@ export default function UpdatePasswordForm() {
                 return true
               },
             })}
-            error={!!errors.newPassword}
-            helperText={errors.newPassword?.message}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword2}
-                    onMouseDown={handleMouseDownPassword2}
-                    edge="end"
-                  >
-                    {showPassword2 ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
+            errors={errors.newPassword}
+            showPassword={showPassword.new}
+            handleClickShowPassword={() => handleClickShowPassword("new")}
+            handleMouseDownPassword={handleMouseDownPassword}
           />
         </Grid>
         <Grid item xs={12} md={6} lg={12}>
-          <TextField
+          <PasswordField
             label="Confirmar contraseña"
-            variant="outlined"
-            fullWidth
-            type={showPassword3 ? "text" : "password"}
-            {...register("confirmPassword", {
+            register={register("confirmPassword", {
               required: "Este campo es requerido",
               validate: (value) => {
                 if (passwordValidations.isPassword(value)) {
@@ -133,22 +91,10 @@ export default function UpdatePasswordForm() {
                 return true
               },
             })}
-            error={!!errors.confirmPassword}
-            helperText={errors.confirmPassword?.message}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword3}
-                    onMouseDown={handleMouseDownPassword3}
-                    edge="end"
-                  >
-                    {showPassword3 ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
+            errors={errors.confirmPassword}
+            showPassword={showPassword.confirm}
+            handleClickShowPassword={() => handleClickShowPassword("confirm")}
+            handleMouseDownPassword={handleMouseDownPassword}
           />
         </Grid>
       </Grid>
