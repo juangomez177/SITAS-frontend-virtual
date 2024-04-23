a91a10e2cc9af11a4af4eb59549ebc9f5d1c0448
lauraT <lauratobon@gmail.com>
2024-04-22 21:35:15 -0500

    fixing deploy

--- app/(home)/profile/edit-profile/page.tsx
+++ app/(home)/profile/edit-profile/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import UpdateForm from "./ui/UpdateForm"
 import { Titles } from "components/ui/Titles"
 
-export function EditProfile({ children }: { children: React.ReactNode }) {
+const EditProfile: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <>
       <Titles title="Actualizar datos" subtitle="¡Comienza a editar el perfil de tu cuenta de Singapur Airlines!" />
