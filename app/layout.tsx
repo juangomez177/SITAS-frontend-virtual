1e9f08c797be1c41d8a82fa421df8cd6447b6eea
Juan Santa <jcsantahurtado@gmail.com>
2024-04-23 09:57:19 -0500

    Pasarelas de pago y finalizar pago con tarjeta de crédito

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
+import Footer from "components/footer/Footer"
+import Header from "components/header/Header"
 import "styles/tailwind.css"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
