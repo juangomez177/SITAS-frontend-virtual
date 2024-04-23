0ba609d45204d3c781493257ac26676c7478e9f7
juan-arismendy <juan.arismendy@udea.edu.co>
2024-04-22 20:57:05 -0500

    rolling bak the edit profile test

--- app/(home)/profile/edit-profile/page.tsx
+++ app/(home)/profile/edit-profile/page.tsx
@@ -1,15 +1,13 @@
-// import Box from "@mui/material/Box"
-// import UpdateForm from "./ui/UpdateForm"
-// import ProfileLayout from "../layout"
-// import { Titles } from "components/ui/Titles"
+import UpdateForm from "./ui/UpdateForm"
+import { Titles } from "components/ui/Titles"
 
-// export function EditProfile({ children }: { children: React.ReactNode }) {
-//   return (
-//     <>
-//       <Titles title="Actualizar datos" subtitle="¡Comienza a editar el perfil de tu cuenta de Singapur Airlines!" />
-//       <UpdateForm />
-//     </>
-//   )
-// }
+export function EditProfile({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Titles title="Actualizar datos" subtitle="¡Comienza a editar el perfil de tu cuenta de Singapur Airlines!" />
+      <UpdateForm />
+    </>
+  )
+}
 
 
